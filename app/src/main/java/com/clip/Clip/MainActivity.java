211d1fe74eae063c018d2ffe4b5f064bad039115
Filipe Melo <filipe.melo@liferay.com>
2022-11-02 13:17:37 -0300

    feat(Create Group): Add group type Spinner

--- app/src/main/java/com/clip/Clip/MainActivity.java
+++ app/src/main/java/com/clip/Clip/MainActivity.java
@@ -3,12 +3,21 @@ package com.clip.Clip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner typeSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeSpinner = findViewById(R.id.type_spinner);
+        ArrayAdapter<CharSequence> typeSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.types_array, android.R.layout.simple_spinner_item);
+        // typeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        typeSpinner.setAdapter(typeSpinnerAdapter);
+
     }
 }
