354394fe59f6db1e5afaa628b121367d6c90cfde
Filipe Melo <filipe.melo@liferay.com>
2022-11-03 21:46:52 -0300

    feat(Create Group): Create GlobalContext

--- app/src/main/java/com/clip/Clip/service/GlobalContext.java
+++ app/src/main/java/com/clip/Clip/service/GlobalContext.java
@@ -1,2 +1,22 @@
-package com.clip.Clip.service;public class GlobalContext {
+package com.clip.Clip.service;
+
+import com.clip.Clip.domain.Group;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalContext {
+    List<Group> groups = new ArrayList<>();
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void addGroup(Group group) {
+        groups.add(group);
+    }
+    private static final GlobalContext holder = new GlobalContext();
+
+    public static GlobalContext getInstance() {return holder;}
+
 }
